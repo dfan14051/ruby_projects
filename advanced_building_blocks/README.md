c50e839933ba90697d094fab567cd3c777efeace
David Fan <dfan14051@gmail.com>
2017-01-09 14:49:39 -0500

    Add advanced_building_blocks and bubble_sort.rb

---
+++ advanced_building_blocks/README.md
@@ -0,0 +1,2 @@
+# advanced_building_blocks
+Advanced Building blocks ruby projects from The Odin Project [Curriculum](http://www.theodinproject.com/courses/ruby-programming/lessons/building-blocks)
---
+++ advanced_building_blocks/bubble_sort.rb
@@ -0,0 +1,51 @@
+def bubble_sort input
+	still_sorting = true
+
+	while still_sorting
+
+		still_sorting = false
+		i, j = 0, 1
+
+		while j < input.length
+
+			if(input[i] > input[j])
+
+				input[i], input[j] = input[j], input[i]
+				still_sorting = true
+
+			end
+
+			i += 1
+			j += 1
+
+		end
+	end
+
+	input
+end
+
+def bubble_sort_by input
+	still_sorting = true
+
+	while still_sorting
+
+		still_sorting = false
+		i, j = 0, 1
+
+		while j < input.length
+
+			if(yield(input[i],input[j]) > 0)
+
+				input[i], input[j] = input[j], input[i]
+				still_sorting = true
+
+			end
+
+			i += 1
+			j += 1
+
+		end
+	end
+
+	input
+end
